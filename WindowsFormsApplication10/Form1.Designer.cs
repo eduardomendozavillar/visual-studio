bde7ca4330f5dc6714720f23cd28129ec06c69aa
eduardo <eduardomendozavillar@gmail.com>
2019-06-28 16:49:34 -0500

    eduardo

---
+++ WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btopen = new System.Windows.Forms.Button();
+            this.btclear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btopen
+            // 
+            this.btopen.Location = new System.Drawing.Point(593, 124);
+            this.btopen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btopen.Name = "btopen";
+            this.btopen.Size = new System.Drawing.Size(100, 28);
+            this.btopen.TabIndex = 0;
+            this.btopen.Text = "open";
+            this.btopen.UseVisualStyleBackColor = true;
+            this.btopen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btclear
+            // 
+            this.btclear.Location = new System.Drawing.Point(593, 160);
+            this.btclear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btclear.Name = "btclear";
+            this.btclear.Size = new System.Drawing.Size(100, 28);
+            this.btclear.TabIndex = 1;
+            this.btclear.Text = "clear";
+            this.btclear.UseVisualStyleBackColor = true;
+            this.btclear.Click += new System.EventHandler(this.btclear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, -14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(585, 332);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 417);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btclear);
+            this.Controls.Add(this.btopen);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btopen;
+        private System.Windows.Forms.Button btclear;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string imagen = openFileDialog1.FileName;
+                    pictureBox1.Image = Image.FromFile(imagen);
+
+                }
+                }
+                catch(Exception){
+                MessageBox.Show("El Archivo Selecsionado No Es Tipo Imagen");
+
+
+            }
+
+        }
+
+        private void btclear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
